eef2506d5cccd7e62376fdc115bbb4a5c423e826
Haomin Kong <a645162@qq.com>
2023-10-15 14:32:19 +0800

    完成主体功能！

--- Nvidia-Tesla-WDDM-Tools/GUI/WPF-DotNetFramework/MainWindow.xaml.cs
+++ Nvidia-Tesla-WDDM-Tools/GUI/WPF-DotNetFramework/MainWindow.xaml.cs
@@ -7,6 +7,9 @@ using System.Windows.Media;
 using HandyControl.Controls;
 
 using SharedProject1;
+using System.Collections.Generic;
+using System;
+using MessageBox = HandyControl.Controls.MessageBox;
 
 namespace NVIDIA_Tesla_WDDM_Tools_WPF_DotNetFramework
 {
@@ -22,54 +25,111 @@ namespace NVIDIA_Tesla_WDDM_Tools_WPF_DotNetFramework
 
         private void ButtonSkins_OnClick(object sender, RoutedEventArgs e)
         {
-        if (e.OriginalSource is Button button)
-        {
-            PopupConfig.IsOpen = false;
-            if (button.Tag is ApplicationTheme tag)
+            if (e.OriginalSource is Button button)
             {
-                ((App)Application.Current).UpdateTheme(tag);
-            }
-            else if (button.Tag is Brush accentTag)
-            {
-                ((App)Application.Current).UpdateAccent(accentTag);
-            }
-            else if (button.Tag is "Picker")
-            {
-                var picker = SingleOpenHelper.CreateControl<ColorPicker>();
-                var window = new PopupWindow
+                PopupConfig.IsOpen = false;
+                if (button.Tag is ApplicationTheme tag)
                 {
-                    PopupElement = picker,
-                    WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                    AllowsTransparency = true,
-                    WindowStyle = WindowStyle.None,
-                    MinWidth = 0,
-                    MinHeight = 0,
-                    Title = "Select Accent Color"
-                };
+                    ((App)Application.Current).UpdateTheme(tag);
+                }
+                else if (button.Tag is Brush accentTag)
+                {
+                    ((App)Application.Current).UpdateAccent(accentTag);
+                }
+                else if (button.Tag is "Picker")
+                {
+                    var picker = SingleOpenHelper.CreateControl<ColorPicker>();
+                    var window = new PopupWindow
+                    {
+                        PopupElement = picker,
+                        WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                        AllowsTransparency = true,
+                        WindowStyle = WindowStyle.None,
+                        MinWidth = 0,
+                        MinHeight = 0,
+                        Title = "Select Accent Color"
+                    };
 
-                picker.SelectedColorChanged += delegate
-                {
-                    ((App)Application.Current).UpdateAccent(picker.SelectedBrush);
-                    window.Close();
-                };
-                picker.Canceled += delegate { window.Close(); };
-                window.Show();
+                    picker.SelectedColorChanged += delegate
+                    {
+                        ((App)Application.Current).UpdateAccent(picker.SelectedBrush);
+                        window.Close();
+                    };
+                    picker.Canceled += delegate { window.Close(); };
+                    window.Show();
+                }
             }
         }
-    }
         #endregion
 
-        private void btn_refresh_Click(object sender, RoutedEventArgs e)
+        #region Component
+
+        private void BtnRefresh_Click(object sender, RoutedEventArgs e)
         {
+            RefreshList();
+        }
+
+        private void BtnModify_Click(object sender, RoutedEventArgs e)
+        {
+            ModifySelectedItems();
+        }
+
+        #endregion
+
+        #region GPU
+
+        private void RefreshList()
+        {
+            ListBoxDevices.Items.Clear();
+
             var gpuList = ToolsClass.ReadGpuList();
 
             foreach (var gpuItem in gpuList)
             {
-                Debug.WriteLine(gpuItem.Item1+" "+gpuItem.Item2);
+                Debug.WriteLine(gpuItem.Item1 + " " + gpuItem.Item2);
+
+                var newItem = new ListBoxItem
+                {
+                    Tag = gpuItem.Item1,
+                    Content = gpuItem.Item2
+                };
+
+                ListBoxDevices.Items.Add(newItem);
+
+            }
+        }
+
+        private void ModifySelectedItems()
+        {
+            var selectedItems = ListBoxDevices.SelectedItems;
+
+            if (selectedItems.Count == 0)
+            {
+                MessageBox.Show
+                (
+                    "您还没有选中任何项目！",
+                    "温馨提醒",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information
+                );
+
             }
 
-            
+            var gpuList = new List<string>();
 
+            foreach (ListBoxItem listBoxItem in selectedItems)
+            {
+                var gpuId = listBoxItem.Tag.ToString();
+                var gpuName = listBoxItem.Content.ToString();
+
+                Debug.WriteLine($"GpuId:{gpuId} GpuName:{gpuName}");
+                //string selectedValue = listBoxItem.ToString();
+                gpuList.Add(gpuId);
+            }
+
+            //ToolsClass.ModifyGpuReg(gpuList);
         }
+
+        #endregion
     }
 }
--- Nvidia-Tesla-WDDM-Tools/Library/SharedProject1/ToolsClass.cs
+++ Nvidia-Tesla-WDDM-Tools/Library/SharedProject1/ToolsClass.cs
@@ -52,11 +52,57 @@ namespace SharedProject1
                 hkeyGpu.Close();
             }
 
+            hkeyGpuList.Close();
+            hkeyLocalMachine.Close();
+
             return gpuNameList;
         }
 
+        public static void ModifyGpuReg(List<string> gpuIdList)
+        {
+            var hkeyLocalMachine = Registry.LocalMachine;
+            var hkeyGpuList =
+                hkeyLocalMachine.OpenSubKey
+                (
+                    GpuListRegPath,
+                    true
+                );
+
+            if (hkeyGpuList == null)
+            {
+                // 如果系统直接没有这个键，那后续就不需要任何操作了！
+                return;
+            }
+
+            foreach (var gpuId in gpuIdList)
+            {
+                if (gpuId.Length == 4)
+                {
+                    var gpuSubKey = hkeyGpuList.OpenSubKey(gpuId, true);
+                    if (gpuSubKey != null)
+                    {
+                        var gpuName = (string)gpuSubKey.GetValue("DriverDesc");
+                        if (gpuName != null)
+                        {
+                            Debug.WriteLine("\n即将开始处理：" + gpuName);
+                            ModifyGpuRegByRegKey(gpuSubKey);
+                        }
+
+                        gpuSubKey.Close();
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine(gpuId+" 输入非法！");
+                }
+            }
+
+            hkeyGpuList.Close();
+            hkeyLocalMachine.Close();
+        }
+
         // 返回值说明：1=成功 0=已经转换，无需转换 -1=失败
-        public static int ModifyGpuReg(RegistryKey gpuRegKey)
+        public static int ModifyGpuRegByRegKey(RegistryKey gpuRegKey)
         {
             //Console.WriteLine((uint)(int)gpuRegKey.GetValue("AdapterType"));
             if (
@@ -85,125 +131,6 @@ namespace SharedProject1
             }
         }
 
-        static void doall()
-        {
-            Console.WriteLine("NVIDIA Tesla 计算卡TTC切换WDDM工具");
-
-
-            RegistryKey hk_lm = Registry.LocalMachine;
-            RegistryKey hk_gpu_list =
-                hk_lm.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}",
-                    true);
-
-            //Console.WriteLine(hk_gpu_list.SubKeyCount);
-            string[] subnameStrings = hk_gpu_list.GetSubKeyNames();
-
-            int max_count = 0;
-            List<(string, string)> gpuNameList = new List<(string, string)>();
-
-            for (int i = 0; i < subnameStrings.Length; i++)
-            {
-                var subNameStr = subnameStrings[i];
-                if (string.IsNullOrEmpty(subNameStr)) { continue; }
-                if (subNameStr.Length != 4) { continue; }
-
-                var hk_gpu = hk_gpu_list.OpenSubKey(subNameStr);
-                if (hk_gpu != null)
-                {
-                    max_count = i;
-                    var gpuName = (string)hk_gpu.GetValue("DriverDesc");
-                    if (gpuName != null)
-                    {
-                        gpuNameList.Add((subNameStr, gpuName));
-                    }
-
-                    hk_gpu.Close();
-                }
-                //else
-                //{
-                //    break;
-                //}
-            }
-
-
-            Console.WriteLine("\n\n显卡列表：");
-            foreach (var gpuName in gpuNameList)
-            {
-                Console.WriteLine(gpuName.Item1 + "\t" + gpuName.Item2);
-                if (gpuName.Item2.ToLower().IndexOf("tesla".ToLower(), StringComparison.Ordinal) != -1)
-                {
-                    Console.WriteLine("\t这是一个NVIDIA Tesla计算卡！");
-                    Console.WriteLine();
-                }
-            }
-
-            Console.WriteLine("\n请输入4位的代码：");
-            string gpuKey = Console.ReadLine();
-            //const string gpuKey = "0001";
-
-            if (gpuKey.Length == 4)
-            {
-                var hk_gpu = hk_gpu_list.OpenSubKey(gpuKey, true);
-                if (hk_gpu != null)
-                {
-                    var gpuName = (string)hk_gpu.GetValue("DriverDesc");
-                    if (gpuName != null)
-                    {
-                        Console.WriteLine("\n即将开始处理：" + gpuName);
-                        handleReg(hk_gpu);
-                    }
-
-                    hk_gpu.Close();
-                }
-            }
-            else
-            {
-                Console.WriteLine("输入非法！");
-            }
-
-
-            //foreach (var item in subnameStrings)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-
-            hk_gpu_list.Close();
-            hk_lm.Close();
-
-
-            Console.WriteLine("");
-            Console.WriteLine("ok!");
-            System.Console.ReadKey();
-        }
-
-        private static void handleReg(RegistryKey hk_gpu)
-        {
-            //Console.WriteLine((uint)(int)gpuRegKey.GetValue("AdapterType"));
-            if ((uint)(int)hk_gpu.GetValue("AdapterType") != 1
-                && (uint)(int)hk_gpu.GetValue("FeatureScore") != 209)
-            {
-                Console.WriteLine("即将转换为WDDM模式！");
-                hk_gpu.SetValue("AdapterType", 1, RegistryValueKind.DWord);
-                hk_gpu.SetValue("FeatureScore", 209, RegistryValueKind.DWord);
-                hk_gpu.SetValue("GridLicensedFeatures", 7, RegistryValueKind.DWord);
-                hk_gpu.SetValue("EnableMsHybrid", 1, RegistryValueKind.DWord);
-            }
-            else
-            {
-                Console.WriteLine("貌似不可以转换！");
-                //Console.WriteLine((int)gpuRegKey.GetValue("AdapterType"));
-                //Console.WriteLine((int)gpuRegKey.GetValue("FeatureScore"));
-                if ((int)hk_gpu.GetValue("FeatureScore") == 209)
-                {
-                    Console.WriteLine("貌似已经工作在WDDM模式下了！");
-                }
-            }
-            //string str16 = string.Format("{0:x}", 209); //转十六进制数
-            //gpuRegKey.SetValue("test111", 209, RegistryValueKind.DWord);
-
-            //gpuRegKey.SetValue("test111121", str16, RegistryValueKind.String);
-        }
 
     }
 }
