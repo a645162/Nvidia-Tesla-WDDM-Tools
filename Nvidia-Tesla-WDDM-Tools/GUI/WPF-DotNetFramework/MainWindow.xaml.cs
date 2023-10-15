fa3e68ccfd5c3a97d313bbbb701bd8f771e3ee51
Haomin Kong <a645162@qq.com>
2023-10-15 16:45:29 +0800

    设计了一下界面，基本搞定！

--- Nvidia-Tesla-WDDM-Tools/GUI/WPF-DotNetFramework/MainWindow.xaml.cs
+++ Nvidia-Tesla-WDDM-Tools/GUI/WPF-DotNetFramework/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using SharedProject1;
 using System.Collections.Generic;
 using System;
 using MessageBox = HandyControl.Controls.MessageBox;
+using System.Windows.Input;
 
 namespace NVIDIA_Tesla_WDDM_Tools_WPF_DotNetFramework
 {
@@ -63,6 +64,15 @@ namespace NVIDIA_Tesla_WDDM_Tools_WPF_DotNetFramework
         #endregion
 
         #region Component
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BtnRefresh.Focus();
+        }
 
         private void BtnRefresh_Click(object sender, RoutedEventArgs e)
         {
@@ -74,9 +84,19 @@ namespace NVIDIA_Tesla_WDDM_Tools_WPF_DotNetFramework
             ModifySelectedItems();
         }
 
+        private void ListBoxDevices_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ModifySelectedItems();
+            }
+        }
+
         #endregion
 
         #region GPU
+
+        private bool refreshed=false;
 
         private void RefreshList()
         {
@@ -96,14 +116,46 @@ namespace NVIDIA_Tesla_WDDM_Tools_WPF_DotNetFramework
 
                 ListBoxDevices.Items.Add(newItem);
 
+                if (gpuItem.Item2.ToLower().Contains("tesla"))
+                {
+                    ListBoxDevices.SelectedItems.Add(newItem);
+                }
             }
+
+            ListBoxDevices.Focus();
         }
 
         private void ModifySelectedItems()
         {
-            var selectedItems = ListBoxDevices.SelectedItems;
+            if (ListBoxDevices.Items.Count == 0)
+            {
+                if (refreshed)
+                {
+                    MessageBox.Show
+                    (
+                        "您已经执行过刷新操作，但是好像您没有任何设备！",
+                        "错误",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error
+                    );
+                }
+                else
+                {
+                    MessageBox.Show
+                    (
+                        "您还没有执行过刷新操作！\n将执行刷新，并处理程序自动选择的项目！",
+                        "温馨提醒",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Information
+                    );
+                    RefreshList();
+                }
+            }
 
-            if (selectedItems.Count == 0)
+            var selectedItems = ListBoxDevices.SelectedItems;
+            var selectedItemsCount = selectedItems.Count;
+
+            if (selectedItemsCount == 0)
             {
                 MessageBox.Show
                 (
@@ -112,6 +164,28 @@ namespace NVIDIA_Tesla_WDDM_Tools_WPF_DotNetFramework
                     MessageBoxButton.OK,
                     MessageBoxImage.Information
                 );
+                return;
+            }
+            else
+            {
+
+                MessageBoxResult result = MessageBox.Show
+                (
+                    $"即将开始处理选中的{selectedItemsCount}个设备",
+                    "温馨提醒",
+                    MessageBoxButton.OKCancel,
+                    MessageBoxImage.Warning
+                );
+
+                if (result == MessageBoxResult.OK)
+                {
+                    Debug.WriteLine("用户确定执行！");
+                }
+                else
+                {
+                    Debug.WriteLine("取消执行！");
+                    return;
+                }
 
             }
 
@@ -131,5 +205,34 @@ namespace NVIDIA_Tesla_WDDM_Tools_WPF_DotNetFramework
         }
 
         #endregion
+
+        #region Menu
+        private void MenuItemRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void MenuItemModify_Click(object sender, RoutedEventArgs e)
+        {
+            ModifySelectedItems();
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItemGithubRepository_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemAboutProgram_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
     }
 }
